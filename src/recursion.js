cb5255f75db63e1a54f2376415f3afc99ff74602
Andrew Hsu <andrewhsu42@gmail.com>
2016-12-25 22:02:11 -0800

    Pass tests for palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -162,6 +162,21 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if(string.length === 0 || string.length === 1) {
+    return true;
+  }
+  var start = string.slice(0, 1);
+  var end = string.slice(string.length - 1);
+  if(start === ' ') {
+    return palindrome(string.slice(1));
+  }
+  if(end === ' ') {
+    return palindrome(string.slice(0, string.length - 1));
+  }
+  if(start.toLowerCase() === end.toLowerCase()) {
+    return palindrome(string.slice(1, string.length - 1));
+  }
+  return false;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
