f6e56b3c3400f2f1e86441e4322373ac19f58562
Andrew Hsu <andrewhsu42@gmail.com>
2016-12-25 22:26:58 -0800

    Pass tests for modulo

--- src/recursion.js
+++ src/recursion.js
@@ -185,6 +185,25 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if(x === 0 && y === 0) {
+    return NaN;
+  }
+  if(x === y) {
+    return 0;
+  }
+  if(y < 0) {
+    return modulo(x, -y);
+  }
+  if(x < 0) {
+    return -modulo(-x, y);
+  }
+  if(x < y) {
+    return x;
+  }
+  if(x - y < y) {
+    return x - y;
+  }
+  return modulo(x - y, y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
